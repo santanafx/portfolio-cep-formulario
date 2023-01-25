7cc48a05eff58be384d4144e42b02082012008a2
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-01-24 22:00:55 -0300

    Lista de adicionar cartao de enderecos

--- js/main.js
+++ js/main.js
@@ -1,28 +1,54 @@
 const botaoSalvar = document.querySelector("#cartao__salvar");
-const cep = document.querySelector("#cep");
+let cep = document.querySelector("#cep");
+const botaoLimpar = document.querySelector("#cartao__limpar");
+let logradouro = document.querySelector("#logradouro");
+let cidade = document.querySelector("#cidade");
+let numero = document.querySelector("#numero");
+
+const endereco = document.querySelector(".endereco__bg");
+let array = [];
 
 const preencherFormulario = (data) => {
-  const logradouro = document.querySelector("#logradouro");
-  const cidade = document.querySelector("#cidade");
   logradouro.value = data.logradouro;
   cidade.value = data.localidade;
 };
 
+function limparFormulario() {
+  cep.value = "";
+  logradouro.value = "";
+  numero.value = "";
+  cidade.value = "";
+}
+
+function salvaFormulario() {
+  endereco.innerHTML += `<div class="endereco__container">
+      <h3>${cidade.value}</h3>
+      <p>${logradouro.value}, ${numero.value}</p>
+      <p class="endereco__cep">${cep.value}</p>
+  </div>`;
+}
+
+botaoLimpar.addEventListener("click", (evento) => {
+  evento.preventDefault();
+  limparFormulario();
+});
+
+botaoSalvar.addEventListener("click", (evento) => {
+  evento.preventDefault();
+  salvaFormulario();
+  limparFormulario();
+});
+
+cep.addEventListener("focusout", () => {
+  pesquisaCep();
+});
+
 async function pesquisaCep() {
   const response = await fetch(`https://viacep.com.br/ws/${cep.value}/json/`);
   const data = await response.json();
-  console.log(data);
   if (data.hasOwnProperty("erro")) {
     cep.value = "CEP não encontrado";
   } else {
     preencherFormulario(data);
   }
 }
-
-cep.addEventListener("focusout", () => {
-  pesquisaCep();
-});
-
-botaoSalvar.addEventListener("click", (evento) => {
-  evento.preventDefault();
-});
