b534decf77d9b0758f0bac168e8d4a2a46c6b43e
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-01-26 17:06:50 -0300

    Botao de apagar lista de enderecos dinamicamente

--- js/salvaFormulario.js
+++ js/salvaFormulario.js
@@ -1,8 +1,27 @@
 export function salvaFormulario() {
-  const endereco = document.querySelector(".endereco__bg");
-  endereco.innerHTML += `<div class="endereco__container">
-        <h3>${cidade.value}</h3>
-        <p>${logradouro.value}, ${numero.value}</p>
-        <p class="endereco__cep">${cep.value}</p>
-    </div>`;
+  const enderecoBg = document.querySelector(".endereco__bg");
+  const primeiraDiv = document.createElement("div");
+
+  primeiraDiv.innerHTML = `<h3>${cidade.value}</h3>
+  <p>${logradouro.value}, ${numero.value}</p>
+  <p class="endereco__cep">${cep.value}</p>`;
+
+  const enderecoContainer = document.querySelector(".endereco__container");
+  const segundaDiv = document.createElement("div");
+  segundaDiv.classList.add("endereco__container");
+  enderecoBg.appendChild(segundaDiv);
+  segundaDiv.appendChild(primeiraDiv);
+
+  segundaDiv.appendChild(botaoRemover());
+}
+
+function botaoRemover() {
+  const button = document.createElement("button");
+  button.classList.add("endereco__remover");
+  button.innerHTML = "Remover";
+
+  button.addEventListener("click", function () {
+    this.parentNode.remove();
+  });
+  return button;
 }
